ee815be375745b0942c8af06e4ba935934d8ff54
Chris Butera <cbutera@pch.com>
2023-01-28 19:04:38 -0500

    adding node version

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,8 @@ const ContactPage = () =>
 	<div>
 		<h2>Contact Me</h2>
 		<p>
-      Contact form under construction.  Looking to get in touch? Shoot me an email at{` `}
+      Contact form under construction. Looking to get in touch? Shoot me an
+      email at{` `}
 			<a href="cbutera484@gmail.com">cbutera484@gmail.com</a>
 		</p>
 	</div>
